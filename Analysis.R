6ee2504325a1cb34d76f6eeb168bfc50dc3dd974
AbigailLoe <agloe@umich.edu>
2023-03-07 15:18:42 -0500

    outline of project added.

--- Analysis.R
+++ Analysis.R
@@ -1,4 +1,9 @@
-
+##### Game Plan ####
+# Decide deltas/hypothesis situations to test
+# Decide correct n
+# determine ideal number of simulations
+# run simulations
+# win.
 
 
 #### Null Settings ####
