3ea40d03e627fe37f236378bb7680a6b4739d803
AbigailLoe <agloe@umich.edu>
2023-03-07 16:06:46 -0500

    preliminary simulation calculations

--- Analysis.R
+++ Analysis.R
@@ -7,6 +7,31 @@
 
 
 #### Null Settings ####
+######## Setting 1 #########
+#p1 = 0, p2 = .15, sig.level = 0.05, power = 0.80
+power.prop.test(n= NULL, p1 = 0, p2 = 0.15,
+                sig.level = 0.05, power= 0.80,
+                alternative = "two.sided")
+# initial n is 48 in each group, 96 total.
+# this 24 in each group at the interim, 48 total
+
+# ---- Determining S ------
+m = 0.02
+S.power = (1.96/m)^2 *.8*(1-.8); S.power
+S.alpha = (1.96/m)^2 *.95*(1-.95); S.alpha
+# implies we need at least 1537 montecarlo runs.
+
+
+
+
+
+######## Setting 2 #########
+
+
+######## Setting 3 #########
+
+
+######## Setting 4 #########
 
 
 #### Alternative Settings ####
