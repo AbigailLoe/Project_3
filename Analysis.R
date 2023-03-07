87006673ea34205476dfdd15f8571b8087718422
AbigailLoe <agloe@umich.edu>
2023-03-07 16:39:26 -0500

    adding to simulation function

--- Analysis.R
+++ Analysis.R
@@ -62,15 +62,26 @@ power.prop.test(n= NULL, p1 = 0.15, p2 = 0.3,
 
 
 trial.sim = function(p.t, p.c, n, S, simulationSettingName){
+  # p.t is proportion of successes we see on treatment
+  # p.c is proprotion fo successes we see on control.
+  # n is the total number of people that we need to enroll across 
+  # both arms of the trial
+  # S is the number of monte carlo replicates.
+  # simulationSetting name is a string that allows us to save the result of the simulation
   results = matrix(NA, nrow= S, ncol = 8)
   colnames(results) = c("replicate", "p.11", "p.12", "T.1",
                         "p.21", "p.22", "T.2", "decision")
   for(r in 1:S){
     # interim analysis
-    #get proportion of success for the controls
-    p.11 = sum(rbinom(n/4, 1, prob = p.c))/(n/4) 
+    #get number of success for the controls
+    p.11 = sum(rbinom(n/4, 1, prob = p.c))
+    #get number of success for the treatment
+    p.12 = sum(rbinom(n/4, 1, prob = p.t))
+    T.1 = ifelse(prop.test(x = c(p.11, p.12), n = c(n/4, n/4))$p.value<0.005,
+                 1, 0)
     
-    #get proportion of success for the treatment
+    
+    
   }
 }
 
